2d3d99a2b6dff8f6f8ff54afa1439c2519189a84
bvadorno <bvadorno@users.noreply.github.com>
2026-04-18 10:27:36 +0000

    Deploying to gh-pages from @ bvadorno/bvadorno.github.io@e1d5895cf8521782e132112f60c3ceba167da4e3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -103,13 +103,6 @@ ninja.data = [{
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/introduction-to-machine-learning/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
